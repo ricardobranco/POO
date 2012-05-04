bf47f7ce3424973dac7fd25d47d476c000e0d355
Ricardo Branco <ricax92@gmail.com>
2012-05-04 21:49:04 +0100

    camioes

---
+++ trasitarios/src/trasitarios/Camiao.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trasitarios;
+
+/**
+ *
+ * @author Ricardo Branco
+ */
+public class Camiao extends Veiculo{
+    private String condutor;
+    private String
+    
+}
